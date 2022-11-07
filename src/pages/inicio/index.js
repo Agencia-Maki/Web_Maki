36ffe3bf2742e20e8316a5a07ed60006775e8a48
Miguel Motta <miguelmotta@MacBook-Pro-de-Miguel.local>
2022-11-07 00:46:27 -0500

    update

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,37 +1,49 @@
 import React from 'react'
-import '../../styles/inicio/style'
+import './style.css'
 
 const Index = () => {
   return (
     <>
-    <div> Estas en un video </div>
-    <div>
-      Estas en una imagen
-    </div>
 
-    <div className="caja">
-      ESTILO MAKI
-    </div>
-    <section>
-      <div>Web</div>
-      <div>Social Media</div>
-    </section>
+    <body>
+      <div className='caja'> Estas en un video </div>
+      <div className='caja'> Estas en una imagen </div>
 
-    <section>
-      <div>
-        Ayudamos a empresas a llegar al siguiente nivel.
-        <small>
-          Algunas de las empresas que confiaron en nosotros
-          para empujarlos a seguir.
-        </small>
-        <small>
-          Conoce más
-        </small>siguiente
-      </div>
-      <div>
-        IMAGENES
-      </div>
-    </section>
+      <section className="caja">
+        <div className='card'>
+          <text className='title'><strong>ESTILO MAKI<span style={{color: "purple"}} >.</span></strong></text><br/>
+          <text><strong>Dualismo entre a inovación y lo tradicional</strong></text><br/>
+          <text>Entendemos lo que tu empresa necesita y optimizamos la manera de obtener resultados</text><br/>
+          <text style={{color: "purple"}}>Conoce nuestros servicios</text>
+        </div>
+        <div className="carrucel">
+          <div className='card'>Web</div>
+          <div className='card'>Social Media</div>
+        </div>
+      </section>
+
+      <section>
+        <div className="caja">
+          <div className='card'>
+            <text className="titleSecondary">
+              <strong>Ayudamos a empresas a llegar <span style={{color: "purple"}} >al siguiente nivel.</span></strong>
+            </text><br/>
+            <text>
+              Algunas de las empresas que confiaron en nosotros
+              para empujarlos a seguir.
+            </text><br/>
+            <small style={{color: "purple"}}>Conoce más</small>
+
+          </div>
+
+          <div>
+              IMAGENES
+          </div>
+        </div>
+      </section>
+
+    </body>
+
     </>
   )
 }
