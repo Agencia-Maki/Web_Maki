85d74d686f43444311834d12c1ce1041e4e0558d
MiguelMottaVilca <miguel.motta@ucsp.edu.pe>
2022-11-07 22:23:30 -0500

    update

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,30 +1,40 @@
 import React from 'react'
-import './style.css'
+// import './style.css'
+import './style.scss'
 
 const Index = () => {
   return (
     <>
 
     <body>
-      <div className='caja'> Estas en un video </div>
-      <div className='caja'> Estas en una imagen </div>
+      {/* <div className='colors: primary'> prueba</div> */}
+      {/* <div className='caja'> Estas en un video </div> */}
+      {/* <div className='caja'> Estas en una imagen </div> */}
 
       <section className="caja">
         <div className='card'>
           <text className='title'><strong>ESTILO MAKI<span style={{color: "purple"}} >.</span></strong></text><br/>
-          <text><strong>Dualismo entre a inovación y lo tradicional</strong></text><br/>
-          <text>Entendemos lo que tu empresa necesita y optimizamos la manera de obtener resultados</text><br/>
+          <text><strong>Dualismo entre la inovación y lo tradicional</strong></text><br/>
+          <text className='parrafo'>Entendemos lo que tu empresa necesita y optimizamos la manera de obtener resultados</text><br/>
           <text style={{color: "purple"}}>Conoce nuestros servicios</text>
         </div>
         <div className="carrucel">
-          <div className='card'>
+          <div className='card' style={{backgroundColor: "#eeeeee"}}>
             <text className='imagen'>IMAGEN</text>
             <text style={{fontSize: "25px"}}>Web</text>
           </div>
-          <div className='card'>
+          <div className='card' style={{backgroundColor: "#eeeeee"}}>
             <text className='imagen'>IMAGEN</text>
             <text style={{fontSize: "25px"}}>Social Media</text>
           </div>
+          {/* <div className='card'>
+            <text className='imagen'>IMAGEN</text>
+            <text style={{fontSize: "25px"}}>Diseño Gráfico</text>
+          </div>
+          <div className='card'>
+            <text className='imagen'>IMAGEN</text>
+            <text style={{fontSize: "25px"}}>Foto y Video</text>
+          </div> */}
         </div>
       </section>
 
@@ -34,19 +44,24 @@ const Index = () => {
             <text className="titleSecondary">
               <strong>Ayudamos a empresas a llegar <span style={{color: "purple"}} >al siguiente nivel.</span></strong>
             </text><br/>
-            <text>
+            <text className='parrafo'>
               Algunas de las empresas que confiaron en nosotros
               para empujarlos a seguir.
             </text><br/>
-            <small style={{color: "purple"}}>Conoce más</small>
+            <text className='color'>Conoce más</text>
 
           </div>
 
-          <div>
-            <text>img 1</text>
-            <text>img 2</text>
-            <text>img 3</text>
-            <text>img 4</text>
+          <div className='grilla'>
+            <text className='logos'>img 1</text>
+            <text className='logos'>img 2</text>
+            <text className='logos'>img 3</text>
+            <text className='logos'>img 4</text>
+            <text className='logos'>img 5</text>
+            <text className='logos'>img 6</text>
+            <text className='logos'>img 7</text>
+            <text className='logos'>img 8</text>
+            <text className='logos'>img 9</text>
           </div>
         </div>
       </section>
