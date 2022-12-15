9fdb9af50cc93d79a0b47b4555c3890507e96865
MiguelMottaVilca <miguel.motta@ucsp.edu.pe>
2022-12-15 11:16:56 -0500

    update

--- src/Layouts/Left/index.js
+++ src/Layouts/Left/index.js
@@ -1,6 +1,7 @@
+import { faShield } from '@fortawesome/free-solid-svg-icons';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import ContactModal from '../../components/Modal/modalContact'
+import Menu from '../Menu/Menu';
 
 import './left.scss'
 
@@ -8,12 +9,10 @@ export const Index = () => {
   
   const [showMenu, setShowMenu] = useState(false)
 
-  const [showModal, setShowModal] = useState(false);
-  const handleClose = () => setShowModal(false);
-  const handleShow = () => setShowModal(true);
-
 	const toggleMenu = () => {
+    console.log("Mostrar menu !")
 		setShowMenu(!showMenu)
+    console.log(showMenu)
 	}
 
   return (
@@ -21,7 +20,7 @@ export const Index = () => {
     
       <div className='maki-menu-container'> 
       
-        <Link className="maki-menu" onClick={() => toggleMenu()} style={{zIndex: 1000}}>
+        <Link className="maki-menu" onClick={() => toggleMenu()} >
           {/* <i className="icon">
             <span className="line"></span>
             <span className="line"></span>
@@ -36,6 +35,8 @@ export const Index = () => {
           </span>
         </Link>
 
+        <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
+
       </div>
 
     </>
@@ -44,82 +45,3 @@ export const Index = () => {
 }
 
 export default Index
-
-
-
-
-{/* <div className="menu-container">
-  <ul className="menu-nav">
-    <li className="nav-item">
-      <Link className="page-home-link" to="/" onClick={() => setShowMenu(false)}>
-        <span className="msk">
-          <span>Inicio</span>
-        </span>
-      </Link>
-    </li>
-
-    <li className="nav-item">
-      <Link className="page-philo-link" to="/proyectos" onClick={() => setShowMenu(false)}>
-        <span className="msk">
-          <span>Proyectos</span>
-        </span>
-      </Link>
-    </li>
-
-    <li className="nav-item">
-      <Link className="page-ideas-link" to="/servicios" onClick={() => setShowMenu(false)}>
-        <span className="msk">
-          <span>Servicios</span>
-        </span>
-      </Link>
-    </li>
-
-    <li className="nav-item">
-      <Link className="page-projects-link" to="/nosotros" onClick={() => setShowMenu(false)}>
-        <span className="msk">
-          <span>Nosotros</span>
-        </span>
-      </Link>
-    </li>
-
-    <li className="nav-item">
-      <Link className="page-careers-link" to="/contactanos" onClick={() => setShowMenu(false)}>
-        <span className="msk">
-          <span>Contáctanos</span>
-        </span>
-      </Link>
-    </li>
-  </ul>
-
-
-  <div className="contact">
-    <address className="addresse">
-      <a href="https://goo.gl/maps/fzzmqZ25Gmz" rel="noreferrer" target="_blank">
-        Urb Real Felipe Mz. A Lte. 1 Cerro Colorado
-      </a>
-    </address> <br />
-    <a href="tel:+51932678688" rel="noreferrer" className="phone">+51932678688</a><br />
-    <a href="mailto:ventas@agenciamaki.com" rel="noreferrer" className="mail">ventas@agenciamaki.com</a>
-  </div>
-
-</div> */}
-
-{/* <button className="newsletterLink" onClick={() => handleShow()}>
-  <span className="block-title">Quieres trabajar con nostros?</span>
-  <span className="next-page-title">Déjanos un mensaje.</span>
-</button> */}
-
-
-{/* <div className="borders">
-<div className="top"></div>
-<div className="bottom"></div>
-<div className="left"></div>
-<div className="right"></div>
-</div> */}
-
-{/* <div className="menuColor" onClick={() => setShowMenu(false)}></div>
-<ContactModal 
-showModal={showModal}
-handleClose={handleClose}
-handleShow={handleShow}
-/> */}
---
+++ src/Layouts/Menu/Menu.js
@@ -0,0 +1,134 @@
+import React, {useState} from 'react'
+import ContactModal from '../../components/Modal/modalContact'
+import { Link } from 'react-router-dom';
+
+import './menu.scss'
+
+const Menu = (props) => {
+
+  console.log("MENU")
+  console.log(props)
+  const { ShowMenu, setShowMenu } = props;
+
+  // console.log(ShowMenu)
+  // console.log(setShowMenu)
+
+  const [showModal, setShowModal] = useState(false);
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  return (
+  <>
+  
+    { showModal && <div className='menu-container'>
+      <ul className=''>
+        <li className=''>
+          <Link to="/" onClick={() => setShowMenu(false)}>
+            <span>
+              <span>Inicio</span>
+            </span>
+          </Link>
+        </li>
+      </ul>
+
+
+      {/* <div className="contact">
+        <address className="addresse">
+          <a href="https://goo.gl/maps/fzzmqZ25Gmz" rel="noreferrer" target="_blank">
+            Urb Real Felipe Mz. A Lte. 1 Cerro Colorado
+          </a>
+        </address> <br />
+        <a href="tel:+51932678688" rel="noreferrer" className="phone">+51932678688</a><br />
+        <a href="mailto:ventas@agenciamaki.com" rel="noreferrer" className="mail">ventas@agenciamaki.com</a>
+      </div> */}
+
+    </div>}
+
+    {/* <button className="newsletterLink" onClick={() => handleShow()}>
+      <span className="block-title">Quieres trabajar con nostros?</span>
+      <span className="next-page-title">Déjanos un mensaje.</span>
+    </button> */}
+
+
+    {/* <div className="borders">
+    <div className="top"></div>
+    <div className="bottom"></div>
+    <div className="left"></div>
+    <div className="right"></div>
+    </div> */}
+
+    {/* <div className="menuColor" onClick={() => setShowMenu(false)}></div> */}
+      {/* <ContactModal 
+      showModal={showModal}
+      handleClose={handleClose}
+      handleShow={handleShow}
+      /> */}
+    </>
+
+  )
+}
+
+export default Menu
+
+
+/*
+
+
+    <div className="menu-container">
+      <ul className="menu-nav">
+        <li className="nav-item">
+          <Link className="page-home-link" to="/" onClick={() => setShowMenu(false)}>
+            <span className="msk">
+              <span>Inicio</span>
+            </span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="page-philo-link" to="/proyectos" onClick={() => setShowMenu(false)}>
+            <span className="msk">
+              <span>Proyectos</span>
+            </span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="page-ideas-link" to="/servicios" onClick={() => setShowMenu(false)}>
+            <span className="msk">
+              <span>Servicios</span>
+            </span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="page-projects-link" to="/nosotros" onClick={() => setShowMenu(false)}>
+            <span className="msk">
+              <span>Nosotros</span>
+            </span>
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="page-careers-link" to="/contactanos" onClick={() => setShowMenu(false)}>
+            <span className="msk">
+              <span>Contáctanos</span>
+            </span>
+          </Link>
+        </li>
+      </ul>
+
+
+      <div className="contact">
+        <address className="addresse">
+          <a href="https://goo.gl/maps/fzzmqZ25Gmz" rel="noreferrer" target="_blank">
+            Urb Real Felipe Mz. A Lte. 1 Cerro Colorado
+          </a>
+        </address> <br />
+        <a href="tel:+51932678688" rel="noreferrer" className="phone">+51932678688</a><br />
+        <a href="mailto:ventas@agenciamaki.com" rel="noreferrer" className="mail">ventas@agenciamaki.com</a>
+      </div>
+
+    </div>
+
+
+*/
